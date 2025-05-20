7898582e961d69dcb0d3d421c12f23229c98936d
Alberto <albertoramosconde1403@gmail.com>
2025-05-20 09:28:32 +0200

    Añadida página index.html con contenido para frontend

--- backend/app.py
+++ backend/app.py
@@ -3,4 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "API operativa con código montado desde o host"
+    return "API operativa con código montado desde el host"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
